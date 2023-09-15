a3ae1018979e6169f3675e3c1f325b5170d1a324
Kawther Merain <141677539+KaeMrn@users.noreply.github.com>
2023-09-15 16:29:08 +0200

    Add files via upload

---
+++ pipboy/js/pipboy.app.js
@@ -0,0 +1,30 @@
+$(document).ready(function (){
+    var weapons = [
+        {"name":"44_pistol",
+        "damage": 48,
+        "fire_rate": 6,
+        "range": 119,
+        "accuracy": 78,
+        "weight":4.3,
+        "value": 99,
+        },
+        {"name":"10mm_Pistol",
+            "damage": 48,
+            "fire_rate": 6,
+            "range": 119,
+            "accuracy": 78,
+            "weight":4.3,
+            "value": 99,
+        },
+        {"name":"assault_rifle",
+            "damage": 48,
+            "fire_rate": 6,
+            "range": 119,
+            "accuracy": 78,
+            "weight":4.3,
+            "value": 99,
+        },
+
+
+    ]
+})
